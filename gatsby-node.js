fedfadc2fcca81bb4a1c988d6ee645e9d3021f5d
onsissond <onsissond@gmail.com>
2025-03-21 20:57:54 +0100

    Add well-known

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,13 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+exports.onCreatePage = ({ page, actions }) => {
+  const { createPage } = actions
+
+  // Ensure apple-app-site-association is served with correct content type
+  if (page.path === '/.well-known/apple-app-site-association') {
+    page.context.contentType = 'application/json'
+  }
+}
+
 // You can delete this file if you're not using it
